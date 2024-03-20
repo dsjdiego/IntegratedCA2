265785b3a50ac9a098ceec82395a16951f983576
sba23045 <152573259+sba23045@users.noreply.github.com>
2024-03-20 19:08:05 +0000

    DB Connection with Netbeans

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -4,10 +4,20 @@
  */
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author User
  */
 public class DBConnection {
-    
+    private static final String URL = "jdbc:mysql://localhost:3306/IntegratedCA2?useSSL=false";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Di101010@";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
