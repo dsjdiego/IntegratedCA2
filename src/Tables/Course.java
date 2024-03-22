71dbe5a6da274e69252c95809fd559ce5e9e405b
sba23045 <152573259+sba23045@users.noreply.github.com>
2024-03-22 18:18:06 +0000

    Create Class Course
    Added Constructions
    Added Getter / Setter

---
+++ src/Tables/Course.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tables;
+
+/**
+ *
+ * @author User
+ */
+public class Course {
+  private int courseId;
+    private int lecturerId;
+    private String courseName;
+    private String courseProgram;
+    private String courseRoom;
+    private boolean isOnline;
+
+    // Constructor
+    public Course(int courseId, int lecturerId, String courseName, String courseProgram, String courseRoom, boolean isOnline) {
+        this.courseId = courseId;
+        this.lecturerId = lecturerId;
+        this.courseName = courseName;
+        this.courseProgram = courseProgram;
+        this.courseRoom = courseRoom;
+        this.isOnline = isOnline;
+    }
+
+    // Getters and Setters
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public int getLecturerId() {
+        return lecturerId;
+    }
+
+    public void setLecturerId(int lecturerId) {
+        this.lecturerId = lecturerId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseProgram() {
+        return courseProgram;
+    }
+
+    public void setCourseProgram(String courseProgram) {
+        this.courseProgram = courseProgram;
+    }
+
+    public String getCourseRoom() {
+        return courseRoom;
+    }
+
+    public void setCourseRoom(String courseRoom) {
+        this.courseRoom = courseRoom;
+    }
+
+    public boolean isOnline() {
+        return isOnline;
+    }
+
+    public void setOnline(boolean isOnline) {
+        this.isOnline = isOnline;
+    }
+}
+
