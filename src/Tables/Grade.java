477f02aacf4f943a3687f68c5c41e35c9ad9261a
sba23045 <152573259+sba23045@users.noreply.github.com>
2024-03-23 01:19:36 +0000

    Update Grade
    Added Constructions
    Added Getter / Setter

--- src/Tables/Grade.java
+++ src/Tables/Grade.java
@@ -9,5 +9,23 @@ package Tables;
  * @author User
  */
 public class Grade {
-    
+    private int gradesId;
+    private int enrollmentId;
+    private String grade;
+
+    public Grade(int gradesId, int enrollmentId, String grade) {
+        this.gradesId = gradesId;
+        this.enrollmentId = enrollmentId;
+        this.grade = grade;
+    }
+
+    public int getGradesId() { return gradesId; }
+    public void setGradesId(int gradesId) { this.gradesId = gradesId; }
+
+    public int getEnrollmentId() { return enrollmentId; }
+    public void setEnrollmentId(int enrollmentId) { this.enrollmentId = enrollmentId; }
+
+    public String getGrade() { return grade; }
+    public void setGrade(String grade) { this.grade = grade; }
 }
+
